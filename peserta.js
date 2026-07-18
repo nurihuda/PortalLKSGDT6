20e02d3d732f9d79f908fb61f997da0421fa3ff0
nurihuda <nurih789@gmail.com>
2026-07-19 01:02:29 +0700

    Create peserta.js with participant data
    Added data for participants of the LKSN Nasional competition.

---
+++ peserta.js
@@ -0,0 +1,24 @@
+// File: peserta.js
+// Data seluruh peserta lomba LKSN Nasional
+window.DATA_PESERTA = [
+  {
+    "no": "01",
+    "nama": "Ahmad Fauzi",
+    "link": "https://drive.google.com/drive/folders/contoh-link-tugas-01"
+  },
+  {
+    "no": "02",
+    "nama": "Budi Santoso",
+    "link": "https://drive.google.com/drive/folders/contoh-link-tugas-02"
+  },
+  {
+    "no": "03",
+    "nama": "Citra Lestari",
+    "link": "https://drive.google.com/drive/folders/contoh-link-tugas-03"
+  },
+  {
+    "no": "04",
+    "nama": "Dewa Made",
+    "link": "https://drive.google.com/drive/folders/contoh-link-tugas-04"
+  }
+];
