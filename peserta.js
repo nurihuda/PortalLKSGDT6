535b642da0281bab117d0e943677f9975d864122
nurihuda <nurih789@gmail.com>
2026-07-19 17:18:27 +0700

    Replace participant data with new sample entries
    Updated participant data with new entries and links.

--- peserta.js
+++ peserta.js
@@ -1,24 +1,14 @@
 // File: peserta.js
-// Data seluruh peserta lomba LKSN Nasional
+// Sampel Data Dummy Skala Nasional Luas
 window.DATA_PESERTA = [
-  {
-    "no": "01",
-    "nama": "Ahmad Fauzi",
-    "link": "https://drive.google.com/drive/folders/contoh-link-tugas-01"
-  },
-  {
-    "no": "02",
-    "nama": "Budi Santoso",
-    "link": "https://drive.google.com/drive/folders/contoh-link-tugas-02"
-  },
-  {
-    "no": "03",
-    "nama": "Citra Lestari",
-    "link": "https://drive.google.com/drive/folders/contoh-link-tugas-03"
-  },
-  {
-    "no": "04",
-    "nama": "Dewa Made",
-    "link": "https://drive.google.com/drive/folders/contoh-link-tugas-04"
-  }
+  { "no": "01", "nama": "Muhammad Nuri Huda", "link": "https://drive.google.com/drive/folders/lksn-gdt-prov-01" },
+  { "no": "02", "nama": "Achmad Zanu", "link": "https://drive.google.com/drive/folders/lksn-gdt-prov-02" },
+  { "no": "03", "nama": "Agnan Fatah", "link": "https://drive.google.com/drive/folders/lksn-gdt-prov-03" },
+  { "no": "04", "nama": "Ivena Laila", "link": "https://drive.google.com/drive/folders/lksn-gdt-prov-04" },
+  { "no": "05", "nama": "Budi Santoso", "link": "https://drive.google.com/drive/folders/lksn-gdt-prov-05" },
+  { "no": "06", "nama": "Zahwa Alya", "link": "https://drive.google.com/drive/folders/lksn-gdt-prov-06" },
+  { "no": "07", "nama": "Akhmad Hafiz Ansyari", "link": "https://drive.google.com/drive/folders/lksn-gdt-prov-07" },
+  { "no": "08", "nama": "Citra Dyah Ariyanti", "link": "https://drive.google.com/drive/folders/lksn-gdt-prov-08" },
+  { "no": "09", "nama": "Rizki Fadilah", "link": "https://drive.google.com/drive/folders/lksn-gdt-prov-09" },
+  { "no": "10", "nama": "Talitha Tarani", "link": "https://drive.google.com/drive/folders/lksn-gdt-prov-10" }
 ];
